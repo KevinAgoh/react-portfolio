45eb67cf442c8237964b2cee62657e7aa9f6ce8f
Kevin Agoh <kevinagoh@hotmail.fr>
2021-06-04 18:01:07 +0200

    there is a bug that I need to fix next time

--- src/components/portfolioList/PortfolioList.jsx
+++ src/components/portfolioList/PortfolioList.jsx
@@ -2,9 +2,12 @@ import "./portfolioList.scss";
 
 import React from 'react'
 
-export default function PortfolioList(title, active, setSelected) {
+export default function PortfolioList(id, title, active, setSelected) {
   return (
-    <li className={active ? "portfolioList active" : "portfolioList"} >
+    <li 
+      className={active ? "portfolioList active" : "portfolioList"} 
+      onClick={()=>setSelected(id)}
+    >
       {title}
     </li>
   )
