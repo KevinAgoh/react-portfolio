6fecd22b2d71aab86b39bed3bf3af0f517bdd0f5
Kevin Agoh <kevinagoh@hotmail.fr>
2021-06-24 19:47:32 +0200

    Stack section, added icons and done some styling

--- src/components/stack/Stack.jsx
+++ src/components/stack/Stack.jsx
@@ -5,20 +5,38 @@ import { FaHtml5 } from 'react-icons/fa';
 import { SiRails } from 'react-icons/si';
 import { DiSass } from 'react-icons/di';
 import { FaWordpress } from 'react-icons/fa';
-
 import "./stack.scss";
 
 export default function Stack() {
   return (
     <div className="stack" id="stack">
       <h1>My Stack</h1>
+      <h4>These are the technologies I have learned and I use for my work</h4>
       <div class="parent">
-        <div class="div1"><FaReact className="icon" /></div>
-        <div class="div2"><DiJavascript1 className="icon" /></div>
-        <div class="div3"><FaHtml5 className="icon" /></div>
-        <div class="div4"><SiRails className="icon" /></div>
-        <div class="div5"><DiSass className="icon" /></div>
-        <div class="div6"><FaWordpress className="icon" /></div>
+        <div class="icon-container">
+          <FaReact className="stack-icon" />
+          <p>React</p>
+        </div>
+        <div class="icon-container">
+          <DiJavascript1 className="stack-icon" />
+          <p>JavaScript</p>
+        </div>
+        <div class="icon-container">
+          <FaHtml5 className="stack-icon" />
+          <p>HTML</p>
+        </div>
+        <div class="icon-container">
+          <SiRails className="stack-icon" />
+          <p>Ruby on Rails</p>
+        </div>
+        <div class="icon-container">
+          <DiSass className="stack-icon" />
+          <p>CSS/Sass</p>
+        </div>
+        <div class="icon-container">
+          <FaWordpress className="stack-icon" />
+          <p>Wordpress</p>
+        </div>
       </div>
     </div>
   )
